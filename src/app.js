0283832f61fc863c58ef76a7d163b660eaeb6110
kevit-mehul-olakiya <119293929+kevit-mehul-olakiya@users.noreply.github.com>
2022-12-13 16:06:21 +0530

    Setup code for deploy

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const forecast = require("./utils/forecast")
 
 
 const app = express()
-
+const port = process.env.PORT  || 3000
 
 const publiceDir = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname,'../src/templet/views')
@@ -90,6 +90,6 @@ app.get('*',(req,res )=>{
  })
 })
 
-app.listen(3000, () => {
-    console.log("Server runnng on 3000");
+app.listen(port, () => {
+    console.log("Server runnng on port" +port);
 })
